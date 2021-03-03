70cef91a9843088223244008c793d0f581cb24d6
rbaron <rbaron@rbaron.net>
2021-03-03 20:31:01 +0100

    Adds some more notes and a new photo

--- code/lib/parasite/parasite/wifi.cpp
+++ code/lib/parasite/parasite/wifi.cpp
@@ -18,7 +18,6 @@ constexpr int kTries = 3;
 // made in that time, return WiFi.status().
 wl_status_t BeginAndWaitForConnection(Stream* serial) {
   int delayed = 0;
-  // TODO: does having a BSSID make the connection faster?
   bool started = WiFi.begin(PRST_WIFI_SSID, PRST_WIFI_PASS);
   serial->printf("[wifi] WiFi.begin(): %d\n", started);
   do {
@@ -42,6 +41,7 @@ wl_status_t BeginAndWaitForConnection(Stream* serial) {
 wl_status_t SetupWiFi(Stream* serial) {
   serial->printf("[wifi] Setting up WiFi. Connecting to %s...\n",
                  PRST_WIFI_SSID);
+  WiFi.disconnect();
   int tries = 0;
   wl_status_t status = WiFi.status();
   do {
@@ -74,7 +74,11 @@ wl_status_t SetupWiFi(Stream* serial) {
   return WiFi.status();
 }
 
-void DisconnectWiFi() { WiFi.disconnect(); }
+void DisconnectWiFi() {
+  WiFi.disconnect();
+  // WiFi.mode(WIFI_OFF);
+  // esp_wifi_stop();
+}
 
 }  // namespace wifi
 }  // namespace parasite
